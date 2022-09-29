58b6e5dca06d873fa7892b0577dfe1014b05d570
Samuel <smarouba2@gmail.com>
2022-09-28 20:18:36 -0400

    commit tree 8c0abcd7ca00e0a1542c5eedd3735cb33ddbc572

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function hasTargetSum(array, target) {
+  let hasMatch = false
   // Write your algorithm here
+  array.forEach((number, index) => {
+    array.forEach((subNumber, subIndex) => {
+      if(subIndex > index ){
+        const sum = number + subNumber
+        if(sum == target) {
+            hasMatch = true
+        }
+      }
+    })
+  });
+  return hasMatch 
 }
 
 /* 
   Write the Big O time complexity of your function here
+  The big O notation is O(n*n)
 */
 
 /* 
   Add your pseudocode here
+  for number in array 
+    for other number in array 
+      if number + other number is equal to the target 
+        then target has a match 
 */
 
 /*
   Add written explanation of your solution here
+  expanation; has targate is function that takes two parameters.
+  the first pramater is an arrary of numbers, the second parameter is 
+  sum of the two of the items in the arrary. We create a const called has match, 
+  by default we set false; then we create a foreEach loop to itterate of 
+  every number in the arrary, we also keep track of the index. We create
+  another forEach loop inside the first forEach loop so that we can compare
+  the current index to all of the other indexes. In the second forEach loop
+  we check to see if the second indext is the greater the first index, and 
+  if it is then we check if the sum  is equal to the target, and if it does
+  we have return a has match. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +56,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log('')
+  console.log("Expecting: false")
+  console.log("=>", hasTargetSum([], 100))
 }
 
 module.exports = hasTargetSum;
